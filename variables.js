78e2fb0a40199fda1ab57ee133d0d990de666c60
sophiefinucane <sophiefinucane1@gmail.com>
2026-09-24 13:04:34 +0100

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,11 @@
+let name = "Sophie"
+let age = 23
+let favouriteColor = "blue"
+let isStudent = true
+
+age = 24
+favouriteColor = "green"
+
+let favouriteBands = ["mazzy star", "slowpulp", "the cure", "radiohead", "Smashing Pumpkins"]
+console.log(favouriteBands[1]);
+console.log(favouriteBands[2]);
